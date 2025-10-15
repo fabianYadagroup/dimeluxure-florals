7f193f8dad3b981510bd0e190c2857da99b769e5
fabianYadagroup <237531977+fabianYadagroup@users.noreply.github.com>
2025-10-15 17:57:47 -0400

    Add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dimeluxure-florals/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
